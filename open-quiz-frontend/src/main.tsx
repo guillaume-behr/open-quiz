bb343c7b055a8e344aed16d8f15f859f40fcac74
Guillaume BEHR <guiguibehr@gmail.com>
2026-07-20 22:51:40 +0200

    move : components files

--- open-quiz-frontend/src/main.tsx
+++ open-quiz-frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client"
 import { BrowserRouter, Route, Routes } from "react-router"
 
 import "./index.css"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { ThemeProvider } from "@/components/theme/theme-provider.tsx"
 
 import "./lib/i18n.ts"
 
