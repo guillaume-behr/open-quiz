899e5f2b9a7ab013433f7ea1710d6be82129792b
Guillaume BEHR <guiguibehr@gmail.com>
2026-07-27 20:37:48 +0200

    add : sudo to deploy docker

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@
 set -eu
 
 git pull --ff-only
-docker compose up --detach --build --remove-orphans
-docker compose ps
+sudo docker compose up --detach --build --remove-orphans
+sudo docker compose ps
