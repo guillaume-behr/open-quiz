6f78f8fe5ab75e16f922adbce56c6aa634fe120e
Guillaume BEHR <guiguibehr@gmail.com>
2026-07-28 01:27:56 +0200

    update : language and theme selection position

--- open-quiz-frontend/src/layouts/main-layout.tsx
+++ open-quiz-frontend/src/layouts/main-layout.tsx
@@ -43,8 +43,10 @@ export function MainLayout() {
                 <Outlet />
             </main>
 
-            <LanguageSelector className="absolute right-1 bottom-1/10" />
-            <ThemeSelector className="absolute right-1 bottom-1/18" />
+            <div className="absolute right-1 bottom-1/18 flex flex-col gap-1">
+                <LanguageSelector />
+                <ThemeSelector />
+            </div>
 
             <footer className="flex h-1/20 w-full items-center justify-between bg-sidebar-primary px-10">
                 <p className="text-sm">v 2026.01</p>
